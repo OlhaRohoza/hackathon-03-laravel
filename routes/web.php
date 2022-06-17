4c8f5355467b577e7c084e97d42a58025a4c6747
OlhaRohoza <olha.rohoza@gmail.com>
2022-06-17 12:25:34 +0200

    link search and detail

--- routes/web.php
+++ routes/web.php
@@ -25,5 +25,4 @@ Route::get('/index', ['App\Http\Controllers\IndexController', 'index_list']);
 
 Route::get('/animals/search/{animal_name}', [AnimalController::class, 'search'])->name('animal.search');
 Route::get('/owners/search/{owner_name}', [OwnerController::class, 'search'])->name('owner.search');
-Route::get('/animals/{animalId}/detail', ['App\Http\Controllers\AnimalController', 'show'])->whereNumber('animalId')->name('animals.detail');
-Route::get('/animals/search/{animal_name}', [AnimalController::class, 'search']);
+Route::get('/animals/detail/{animalId}', ['App\Http\Controllers\AnimalController', 'show'])->whereNumber('animalId')->name('animals.detail');
